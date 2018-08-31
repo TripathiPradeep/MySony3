e2d42b5398c1b9af28df242fc405081a28a8859b
7000017510 <7000017510@INSISCILT-3602.ap.sony.com>
2018-08-31 14:26:56 +0530

    Third commit

---
+++ Assignment/src/Authenticate/AuthenticateServlet.java
@@ -0,0 +1,55 @@
+package Authenticate;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AuthenticateServlet
+ */
+@WebServlet("/AuthenticateServlet")
+public class AuthenticateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AuthenticateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		String userid = request.getParameter("username");
+
+		String pwd = request.getParameter("password");
+
+		if(userid.equalsIgnoreCase("pradeep") && pwd.equalsIgnoreCase("12345")){
+
+		response.getWriter().println("Welcome"+username);
+
+		}else{
+
+		response.getWriter().println("Invalid username / password");
+
+		}
+ 
+	}
+
+}
